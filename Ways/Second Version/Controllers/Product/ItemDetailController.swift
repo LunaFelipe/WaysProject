990832067fc18fe5d02fd99deab47a7246efbc57
Felipe Luna <felipe.luna.tersi@gmail.com>
2019-09-19 15:41:30 -0300

    tamanho da imagem correta na scroll view e page control funcionando

--- Ways/Second Version/Controllers/Product/ItemDetailController.swift
+++ Ways/Second Version/Controllers/Product/ItemDetailController.swift
@@ -143,34 +143,9 @@ class ItemDetailController: UITableViewController {
         
     }
     
-    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-        let imageView = sender.view as! UIImageView
-        let newImageView = UIImageView(image: imageView.image)
-        newImageView.frame = UIScreen.main.bounds
-        newImageView.backgroundColor = .black
-        newImageView.contentMode = .scaleAspectFit
-        newImageView.isUserInteractionEnabled = true
-        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
-        newImageView.addGestureRecognizer(tap)
-        self.view.addSubview(newImageView)
-        self.navigationController?.isNavigationBarHidden = true
-        self.tabBarController?.tabBar.isHidden = true
-    }
-    
-    @objc func dismissFullscreenImage(_ sender: UITapGestureRecognizer) {
-        self.navigationController?.isNavigationBarHidden = false
-        self.tabBarController?.tabBar.isHidden = false
-        sender.view?.removeFromSuperview()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
-//        tap.cancelsTouchesInView = false
-//        view1.addGestureRecognizer(tap)
-//        view2.addGestureRecognizer(tap)
-//        view.addGestureRecognizer(tap)
+    
         
         // numero de paginas que vao aparecer com o page control
         pageView.currentPage = 0
